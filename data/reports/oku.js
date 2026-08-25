33334821bc51fa0204172f73ec5ad4546528549c
Mickeymik95 <mickeymikapple@gmail.com>
2026-08-26 06:31:12 +0800

    ubah no pintu tren

--- data/reports/oku.js
+++ data/reports/oku.js
@@ -32,8 +32,8 @@ export const oku = {
     {
       name: "door",
       label: "DOOR",
-      type: "number",
-      placeholder: "CTH : 6",
+      type: "select",
+      options: ["1", "2", "3", "4", "5","6","7","8","9","10","11","12"],
     },
   ],
 
